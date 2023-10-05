02718567a0f04dcf7d5d6fe5d9ac24791672856d
SushmithaMallesh <sushdec6@gmail.com>
2023-10-05 12:40:37 +0530

    restrict file type upload to pdf

--- app/routes/upload.py
+++ app/routes/upload.py
@@ -6,10 +6,19 @@ from pathlib import Path
 LOCAL_STORAGE_SOURCE_DIRECTORY = "tmp"
 
 
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from pathlib import Path
+
+LOCAL_STORAGE_SOURCE_DIRECTORY = "tmp"
+
+
 def upload_file(session_id: str, file: UploadFile = File(...)):
     # Create a directory for the session if doesn't exist
     session_directory = Path(LOCAL_STORAGE_SOURCE_DIRECTORY) / session_id
     session_directory.mkdir(parents=True, exist_ok=True)
+
+    if file.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="Only PDF files are allowed!")
 
     # Save uploaded file to the session directory
     file_path = session_directory / file.filename
